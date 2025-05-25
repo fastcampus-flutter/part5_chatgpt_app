d9cfb27c0b5b39e363371be8a65dbfc750c4255a
Dreamwalker <aristojeff@gmail.com>
2025-05-25 10:23:36 +0900

    update android project setup

--- android/app/src/main/kotlin/com/fastcampus/flutter_chatgpt_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/fastcampus/flutter_chatgpt_app/MainActivity.kt
@@ -2,5 +2,4 @@ package com.fastcampus.flutter_chatgpt_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
